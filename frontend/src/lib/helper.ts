8eb5fc06c6eb3dd1bc4b36c5b3e09f2a9aeb7b96
anuj-negi-dev <techie.anujnegi@gmail.com>
2025-10-31 10:31:22 +0530

    add helper file

--- frontend/src/lib/helper.ts
+++ frontend/src/lib/helper.ts
@@ -3,6 +3,5 @@ import { useSocket } from "@/hooks/use-socket";
 export const isUserOnline = (userId: string) => {
   if (!userId) return false;
   const { onlineUsers } = useSocket.getState();
-  console.log("Online users from helper", onlineUsers);
   return onlineUsers.includes(userId);
 };
